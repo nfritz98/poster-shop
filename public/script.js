d127c95f69a444ff572737e1704bcc8d5e05c44d
Nicole Fritz <n.fritz@netz98.de>
2019-12-03 08:30:42 +0100

    Video 21, add keys to product-items

--- public/script.js
+++ public/script.js
@@ -4,13 +4,16 @@ new Vue({
         total: 0,
         products: [
             {
-                title: 'Product 1'
+                title: 'Product 1',
+                id: 1
             },
             {
-                title: 'Product 2'
+                title: 'Product 2',
+                id: 2
             },
             {
-                title: 'Product 3'
+                title: 'Product 3',
+                id: 3
             }
         ]
     },
