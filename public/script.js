1de14401ebbcc7124d5839d120bc447569f0cb03
Nicole Fritz <n.fritz@netz98.de>
2019-12-03 09:59:32 +0100

    Video 26, adding products to cart + updating total

--- public/script.js
+++ public/script.js
@@ -5,21 +5,26 @@ new Vue({
         products: [
             {
                 title: 'Product 1',
-                id: 1
+                id: 1,
+                price: 9.99
             },
             {
                 title: 'Product 2',
-                id: 2
+                id: 2,
+                price: 9.99
             },
             {
                 title: 'Product 3',
-                id: 3
+                id: 3,
+                price: 9.99
             }
-        ]
+        ],
+        cart: []
     },
     methods:{
-        addToCart: function(){
-            this.total += 9.99
+        addToCart: function(product){
+            this.total += product.price;
+            this.cart.push(product);
         }
     }
 });
